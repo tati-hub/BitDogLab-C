c80c77f65e71ba62b59c2a8f08c49b88f13a36f5
Lucas Oliver <lucas@oliver.adm.br>
2024-12-08 03:18:33 -0300

    Primeira versão funcional do exemplo de código do SSD1306

---
+++ ssd1306/inc/ssd1306.c
@@ -0,0 +1,77 @@
+#include "./ssd1306.h""
+
+void ssd1306_init(ssd1306_t *ssd, uint8_t width, uint8_t height, bool external_vcc, uint8_t address, i2c_inst_t *i2c) {
+  ssd->width = width;
+  ssd->height = height;
+  ssd->pages = height / 8U;
+  ssd->address = address;
+  ssd->i2c_port = i2c;
+  ssd->bufsize = ssd->pages * ssd->width + 1;
+  ssd->ram_buffer = calloc(ssd->bufsize, sizeof(uint8_t));
+  ssd->ram_buffer[0] = 0x40;
+  ssd->port_buffer[0] = 0x80;
+}
+
+void ssd1306_config(ssd1306_t *ssd) {
+  ssd1306_command(ssd, SET_DISP | 0x00);
+  ssd1306_command(ssd, SET_MEM_ADDR);
+  ssd1306_command(ssd, 0x01);
+  ssd1306_command(ssd, SET_DISP_START_LINE | 0x00);
+  ssd1306_command(ssd, SET_SEG_REMAP | 0x01);
+  ssd1306_command(ssd, SET_MUX_RATIO);
+  ssd1306_command(ssd, HEIGHT - 1);
+  ssd1306_command(ssd, SET_COM_OUT_DIR | 0x08);
+  ssd1306_command(ssd, SET_DISP_OFFSET);
+  ssd1306_command(ssd, 0x00);
+  ssd1306_command(ssd, SET_COM_PIN_CFG);
+  ssd1306_command(ssd, 0x12);
+  ssd1306_command(ssd, SET_DISP_CLK_DIV);
+  ssd1306_command(ssd, 0x80);
+  ssd1306_command(ssd, SET_PRECHARGE);
+  ssd1306_command(ssd, 0xF1);
+  ssd1306_command(ssd, SET_VCOM_DESEL);
+  ssd1306_command(ssd, 0x30);
+  ssd1306_command(ssd, SET_CONTRAST);
+  ssd1306_command(ssd, 0xFF);
+  ssd1306_command(ssd, SET_ENTIRE_ON);
+  ssd1306_command(ssd, SET_NORM_INV);
+  ssd1306_command(ssd, SET_CHARGE_PUMP);
+  ssd1306_command(ssd, 0x14);
+  ssd1306_command(ssd, SET_DISP | 0x01);
+}
+
+void ssd1306_command(ssd1306_t *ssd, uint8_t command) {
+  ssd->port_buffer[1] = command;
+  i2c_write_blocking(
+    ssd->i2c_port,
+    ssd->address,
+    ssd->port_buffer,
+    2,
+    false
+  );
+}
+
+void ssd1306_send_data(ssd1306_t *ssd) {
+  ssd1306_command(ssd, SET_COL_ADDR);
+  ssd1306_command(ssd, 0);
+  ssd1306_command(ssd, ssd->width - 1);
+  ssd1306_command(ssd, SET_PAGE_ADDR);
+  ssd1306_command(ssd, 0);
+  ssd1306_command(ssd, ssd->pages - 1);
+  i2c_write_blocking(
+    ssd->i2c_port,
+    ssd->address,
+    ssd->ram_buffer,
+    ssd->bufsize,
+    false
+  );
+}
+
+void ssd1306_pixel(ssd1306_t *ssd, uint8_t x, uint8_t y, bool value) {
+  uint16_t index = (y >> 3) + (x << 3) + 1;
+  uint8_t pixel = (y & 0b111);
+  if (value)
+    ssd->ram_buffer[index] |= (1 << pixel);
+  else
+    ssd->ram_buffer[index] &= ~(1 << pixel);
+}
---
+++ ssd1306/ssd1306.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+
+int main() {
+  stdio_init_all();
+
+  // I2C Initialisation. Using it at 400Khz.
+  i2c_init(I2C_PORT, 400*1000);
+  
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C_SDA);
+  gpio_pull_up(I2C_SCL);
+
+  ssd1306_t ssd;
+
+  ssd1306_init(&ssd, WIDTH, HEIGHT, false, 0x3C, I2C_PORT);
+  ssd1306_config(&ssd);
+
+  ssd1306_send_data(&ssd);
+
+  ssd1306_pixel(&ssd, 64, 32, true);
+
+  ssd1306_send_data(&ssd);
+
+  while (true) {
+    sleep_ms(1000);
+  }
+}
